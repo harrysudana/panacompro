7332f31f996dd6b9de049289f6dbd7faf6bb3557
harrysudana <harrysudana@gmail.com>
2014-02-21 17:08:01 +0800

    update

--- app/Modules/Administration/Controllers/Home.php
+++ app/Modules/Administration/Controllers/Home.php
@@ -1,12 +1,16 @@
 <?php
-namespace Modules\ModSample\Controllers;
-use Resources;
+namespace Modules\Administration\Controllers;
+use Resources, Libraries;
 
 class Home extends Resources\Controller
 {
     public function __construct()
     {
         parent::__construct();
+        
+        $this->auth = new Libraries\Auth;
+
+        $this->auth->allow();
     }
 
     public function index()
